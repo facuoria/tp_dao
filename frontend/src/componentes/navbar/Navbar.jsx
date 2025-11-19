208399e94b5548121f6a6057fcadb2ef96155507
valereineri <95985@sistemas.frc.utn.edu.ar>
2025-11-18 21:17:43 -0300

    arregle navbar y paginas

--- frontend/src/componentes/navbar/Navbar.jsx
+++ frontend/src/componentes/navbar/Navbar.jsx
@@ -1,25 +1,24 @@
 import { Link } from "react-router-dom";
-
-
 export default function Navbar() {
-return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
-<a className="navbar-brand fw-bold" href="/">Turnero Médico</a>
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-primary px-4"
+      style={{ position: "relative", zIndex: 9999 }}
+    >
+      <Link className="navbar-brand fw-bold" to="/">Turnero Médico</Link>
 
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-<span className="navbar-toggler-icon"></span>
-</button>
-
-
-<div className="collapse navbar-collapse" id="navbarNav">
-<ul className="navbar-nav ms-auto">
-<li className="nav-item"><Link className="nav-link" to="/pacientes">Pacientes</Link></li>
-<li className="nav-item"><Link className="nav-link" to="/medicos">Médicos</Link></li>
-<li className="nav-item"><Link className="nav-link" to="/especialidades">Especialidades</Link></li>
-<li className="nav-item"><Link className="nav-link" to="/recetas">Recetas</Link></li>
-</ul>
-</div>
-</nav>
-);
+      <div className="navbar-collapse show" id="navbarNav" style={{ display: "block" }}>
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item"><Link className="nav-link" to="/pacientes">Pacientes</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/medicos">Médicos</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/especialidades">Especialidades</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/recetas">Recetas</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
